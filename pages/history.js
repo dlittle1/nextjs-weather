e2aeecd15f4d4b2bd51982a61afff8bab11b0239
Dylan Little <dylan.c.little@gmail.com>
2022-05-06 13:35:42 -0600

    create page for showing local storage weather data

---
+++ pages/history.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+const History = () => {
+  const [weatherHistory, setWeatherHistory] = useState([]);
+
+  useEffect(() => {
+    setWeatherHistory(
+      localStorage.weatherHistory ? JSON.parse(localStorage.weatherHistory) : []
+    );
+  }, []);
+
+  return (
+    <div
+      className='d-flex justify-content-center align-items-center p-3'
+      style={{ minHeight: '100vh' }}
+    >
+      <div>
+        <h2>My Weather History</h2>
+        <div className='mt-5'>
+          {weatherHistory.length > 0 ? (
+            weatherHistory.map((weather, index) => {
+              return (
+                <div
+                  key={index}
+                  className='card mb-3'
+                  style={{ width: '450px' }}
+                >
+                  <div className='card-body text-dark'>
+                    <h5 className='card-title '>
+                      {weather.city} - {weather.date}
+                    </h5>
+                    <small>{weather.time}</small>
+                    <hr />
+                    <p className='card-text'>
+                      <span className='font-weight-bold'>Temperature: </span>
+                      {weather.temperature}
+                      <sup>°C</sup>
+                    </p>
+                    <p className='card-text'>
+                      <span className='font-weight-bold'>Condition: </span>
+                      {weather.description}
+                    </p>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <p>Nothing to see here... Yet.</p>
+          )}
+          <Link href='/'>
+            <button className='btn btn-primary border-0 history-btn px-4 py-3 ms-auto'>
+              Home
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default History;
